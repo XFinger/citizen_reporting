ac8ac4d3764cf687cd19914999af27cbc62975d9
xfinger <xfinger@gmail.com>
2016-12-05 23:29:53 -0500

    config/configatron

---
+++ config/configatron/defaults.rb
@@ -0,0 +1,8 @@
+
+
+
+#configatron environmental variables - keeps secrets secret
+
+
+configatron.access_token = 'sq0atp-ZXk9o2oDuSPSVjp-ZqPUKw' #your square access_token
+configatron.location_id = '8K6Z43W5S5ETW'				   #your square locationn_id
