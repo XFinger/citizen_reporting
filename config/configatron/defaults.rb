328a6895a03e72488efb3783ca0429157185fba3
xfinger <xfinger@gmail.com>
2017-03-02 18:17:57 -0500

    config

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -6,5 +6,4 @@
 
 #configatron.access_token = 'square access token'
 #configatron.location_id = 'square location id'
-configatron.access_token = 'sq0atp-ZXk9o2oDuSPSVjp-ZqPUKw'
-configatron.location_id = '8K6Z43W5S5ETW'
+
